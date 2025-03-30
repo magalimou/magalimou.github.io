2469162042dc93d1cb00a08dd050c6867dcac833
magali <maguimourino@gmail.com>
2025-03-30 12:52:37 -0300

    feat: add header and hero sections

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,15 @@
 import React from 'react'
 import './styles/global.css'
 
-import Button from './components/ui/Button'
 import Header from './components/layout/Header'
-import { faArrowDown } from '@fortawesome/free-solid-svg-icons'
-
-
-
+import Hero from './components/sections/Hero'
 
 function App() {
 
   return (
     <>
       <Header />
-      <p>Hi there, Im Magali</p>
-      <h1>Software Developer</h1>
-      <p>
-        Click on the Vite and React logos to learn more
-      </p>
-      <div >
-      <Button text="Download CV" icon={faArrowDown} className='btn' />
-      <Button text="" icon={faArrowDown} className='btn-icon' />
-      </div>
+      <Hero />
     </>
   )
 }
---
+++ src/components/sections/Hero.jsx
@@ -0,0 +1,36 @@
+import React from "react";
+import Button from "../ui/Button";  
+
+import profilePhoto from "../../assets/profile-photo.png";
+import { faEnvelope } from '@fortawesome/free-solid-svg-icons'
+import { faArrowDown } from '@fortawesome/free-solid-svg-icons'
+import { faLinkedinIn } from "@fortawesome/free-brands-svg-icons";
+import { faGithub } from "@fortawesome/free-brands-svg-icons";
+
+const Hero = () => {
+    return (
+        <section className="px-40 py-30" >
+            <div className="flex items-center justify-between">
+                <div>
+                <h2 className="text-4xl font-medium mb-4">Hi there, I'm Magali 👋</h2>
+                <h1 className="text-7xl font-semibold mb-5">Software Developer</h1>
+                <p className=" text-xl">I'm a software developer specializing in building (and sometimes designing)</p>
+                <p className=" text-xl">high-performing digital experiences.</p>
+                <p className=" text-xl">I enjoy working on both backend and frontend development.</p>
+                </div>
+                <div className="w-xs max-w-md">
+                    <img className="w-full h-auto  rounded-full blur-shadow" src={profilePhoto} alt="" />
+                </div>
+            </div>
+            <div className="flex items-center gap-4 justify-left">
+                <Button text="Download CV" icon={faArrowDown} className='btn' />
+                <Button text="" icon={faLinkedinIn} className='btn-icon' />
+                <Button text="" icon={faGithub} className="btn-icon" />
+                <Button text="" icon={faEnvelope} className="btn-icon" />
+            </div>
+            <div className="circle-background-2"></div>
+        </section>
+    );
+}
+
+export default Hero;
