737f7e0c1de3ba16751203eb900c5c3b5b0d38a6
magali <maguimourino@gmail.com>
2025-04-04 03:29:59 -0300

    fix: fix side project card layout

--- src/components/sections/SideProjects.jsx
+++ src/components/sections/SideProjects.jsx
@@ -2,28 +2,29 @@ import SideProjectCard from "../ui/SideProjectCard";
 
 const sideProjects = [
     {
-    title: "PokeMarket",
-    description: "A marketplace for buying, selling, and trading Pokémon cards. Integrates an external Pokémon card API to fetch real-time card data. Focused on clean code, modularity, and maintainability.",
-    techStack: ["Java", "OOP", "JSON File"],
-    link: "#",
-    },
+        title: "PokeMarket",
+        description: "A marketplace for buying, selling, and trading Pokémon cards. Integrates an external Pokémon card API to fetch real-time card data. Focused on clean code, modularity, and maintainability.",
+        techStack: ["Java", "OOP", "JSON File"],
+        repoLink: "https://github.com/MateoCunsolo/pokemarket",
+        },
     {
         title: "Taskit",
         description: "A task management app designed to help users stay organized and track their daily tasks. Features task creation, editing, and completion tracking. Built with a React frontend and a Node.js Express backend.",
         techStack: ["Node.js", "Mysql", "React", "Javascript"],
-        link: "#",
+        liveLink: "https://taskit-frontend-three.vercel.app/",
     },
     {
-    title: "World Cup fixture",
-    description: "Fixture for the FIFA World Cup and results simulator. Simulates match outcomes based on predefined probabilities for each team, calculating results based on each country's probability data.",
-    techStack: ["C", "Pointers", "Structs"],
-    link: "#",
+        title: "World Cup fixture",
+        description: "Fixture for the FIFA World Cup and results simulator. Simulates match outcomes based on predefined probabilities for each team, calculating results based on each country's probability data.",
+        techStack: ["C", "Pointers", "Structs"],
+        repoLink: "https://github.com/magalimou/TP-FINAL-LAB-II",
     },
     {
-    title: "Home Hive",
-    description: "A web platform for browsing and managing real estate properties. Features real-time weather integration, displaying weather conditions for each property location.",
-    techStack: ["HTML", "CSS", "JavaScript", "Figma"],
-    link: "#",
+        title: "Home Hive",
+        description: "A web platform for browsing and managing real estate properties. Features real-time weather integration, displaying weather conditions for each property location.",
+        techStack: ["HTML", "CSS", "JavaScript", "Figma"],
+        repoLink: "https://github.com/magalimou/Real-Estate-Project",
+        liveLink:"https://magalimou.github.io/Real-Estate-Project/"
     },
    
 ];
@@ -33,11 +34,12 @@ const SideProjects = () => {
     return (
         <section className="px-6 sm:px-12 md:px-20 lg:px-40 xl:px-60 pb-10">
         <h2 className="text-2xl sm:text-3xl md:text-4xl lg:text-4xl font-medium mb-6 sm:mb-8 md:mb-10">Other projects I’ve worked on</h2>
-        <div className="grid grid-cols-1 md:grid-cols-2 gap-8">
+        <div className="grid grid-cols-1 md:grid-cols-2 gap-7">
             {sideProjects.map((project, index) => (
                 <SideProjectCard key={index} {...project} />
             ))}
         </div>
+        <div className="circle-background-purple top-[420%] left-[50%]"></div>
         </section>
     );
 }
--- src/components/ui/SideProjectCard.jsx
+++ src/components/ui/SideProjectCard.jsx
@@ -3,14 +3,29 @@ import React from "react";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import {faArrowUpRightFromSquare} from "@fortawesome/free-solid-svg-icons";
 import {faFile} from "@fortawesome/free-regular-svg-icons";
+import { faGithub } from "@fortawesome/free-brands-svg-icons";
+import {faCode} from "@fortawesome/free-solid-svg-icons";
+
 
 
 const SideProjectCard = ({ title, description, techStack, repoLink, liveLink }) => {
     return (
-      <div className="bg-[#1C1222] p-9 shadow-md ">
+      <div className="bg-[#1C1222] p-9 shadow-md hover:shadow-xl/30 rounded-lg transition duration-300 ease-in-out transform hover:-translate-y-1 hover:shadow-gray-400/30">
         <div className="flex justify-between items-center">
             <FontAwesomeIcon className="text-[#FFA56F] text-4xl" icon={faFile}></FontAwesomeIcon>
-            <FontAwesomeIcon className="text-2xl" icon={faArrowUpRightFromSquare} />
+            <div className="gap-5 flex items-center">
+              {repoLink && (
+              <a href={repoLink} target="_blank" rel="noopener noreferrer">
+              <FontAwesomeIcon className="text-2xl hover:text-orange-300 transition" icon={faCode} />
+              </a>
+              )}
+
+              {liveLink && (
+                <a href={liveLink} target="_blank" rel="noopener noreferrer">
+                  <FontAwesomeIcon className="text-2xl hover:text-orange-300 transition" icon={faArrowUpRightFromSquare} />
+                </a>
+              )}
+            </div>
         </div>
         <div>
             <h3 className="text-white text-2xl font-semibold mt-4">{title} </h3>
