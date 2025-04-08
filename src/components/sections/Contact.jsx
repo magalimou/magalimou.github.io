10987cbd5c5db8364b525bdb525c5c8a44dc958a
magali <maguimourino@gmail.com>
2025-04-07 23:59:13 -0300

    fix: make contact section responsive

--- src/components/sections/Contact.jsx
+++ src/components/sections/Contact.jsx
@@ -4,9 +4,9 @@ import { faCopy } from "@fortawesome/free-regular-svg-icons";
 
 const Contact = () => { 
     return (
-        <section id="contact" className="px-60 py-30 text-center">
+        <section id="contact" className="px-6 py-20 sm:px-12 md:px-20 lg:px-40 xl:px-60 text-center">
             <h2 className="text-3xl sm:text-4xl md:text-5xl lg:text-5xl font-semibold mb-5">Let’s Connect</h2>
-            <p className="lg:text-lg md:text-base sm:text-base px-60 mb-6">I'm currently looking for new opportunities as a software developer. Whether you have a project, a job opportunity, or just want to say hi, feel free to reach out!</p>
+            <p className="lg:text-lg md:text-base sm:text-base px-6 sm:px-12 lg:px-25 mb-6">I'm currently looking for new opportunities as a software developer. Whether you have a project, a job opportunity, or just want to say hi, feel free to reach out!</p>
             <div className="flex flex-col sm:flex-row justify-center items-center gap-3">
             <a href="mailto:maguimourino@gmail.com">
                 <Button text="Say Hi" className="btn" />
